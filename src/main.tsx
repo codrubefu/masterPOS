35d847d0de332ac6c8719c47b43ac97d670f51c7
Codrut Befu <codrut_befu@yahoo.com>
2025-11-04 23:20:14 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./app/routes";
 import "./styles/globals.css";
 
-const history = createBrowserHistory();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-  <HistoryRouter history={history} future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AppRoutes />
-    </HistoryRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
