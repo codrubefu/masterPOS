3ad545ad85a39fc678ebb3918d389b462bdbb468
Codrut Befu <codrut_befu@yahoo.com>
2025-11-20 21:36:50 +0200

    configs

--- src/app/configLoader.ts
+++ src/app/configLoader.ts
@@ -2,8 +2,20 @@ let configCache: any = null;
 
 export async function getConfig() {
   if (configCache) return configCache;
-  const configResp = await fetch('/config.json');
-  if (!configResp.ok) throw new Error('Could not load config.json');
+  
+  let configFile: string;
+  const hostname = window.location.hostname;
+  
+  if (hostname.includes('192.168.1.35')) {
+    configFile = '/config-local.json';
+  } else if (hostname.includes('localhost')) {
+    configFile = '/config-dev.json';
+  } else {
+    configFile = '/config-web.json';
+  }
+  
+  const configResp = await fetch(configFile);
+  if (!configResp.ok) throw new Error(`Could not load ${configFile}`);
   configCache = await configResp.json();
   return configCache;
 }
